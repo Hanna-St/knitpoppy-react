86b66bbad524ef2dc65f8bd1a17ef56f66118313
Hanna Stsiapanava <stepanova.anna0103@gmail.com>
2022-11-21 21:56:20 -0600

    Configuring

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './fonts/Amatic_SC/AmaticSC-Bold.ttf';
+import './fonts/Quicksand/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkP8o58i-wjwxUD2GF9Zc.woff2';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -17,7 +21,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
